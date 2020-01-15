ec0778ebc8e338e42e2949835fd3f064267b4bb5
vladislavglad <vladglad.996@gmail.com>
2020-01-14 22:23:38 -0500

    proxy capability is added

--- server.py
+++ server.py
@@ -1,41 +1,73 @@
 import socket
 import dnslib 
 
-server_address = ('', 53)
+# create a socket to resieve queries. 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ('', 53)
 server_socket.bind(server_address)
-print(f'Server is listening on port: {server_address[1]}\n')
 # server_socket.listen(1)
+print(f'Server is listening on port: {server_address[1]}\n')
+
+# create a socket to send queries to external DNS server.
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.settimeout(10)
+# use any public DNS server you like; I use Google's.
+DNS_address = ('8.8.8.8', 53)
 
 while True:
     try:
-        packet, address = server_socket.recvfrom(1024)
-        print(f'Recieved Packet: \n{packet}\n')
-        parsed_packet = dnslib.DNSRecord.parse(packet)
-        print(f'Parsed Packet: \n{parsed_packet}\n')
 
-        question_section = parsed_packet.get_q()
-        # print(f'Quesiton Section: \n{question_section}\n')
+        # recieve query from an external socket.
+        query, client_address = server_socket.recvfrom(1024)
+        print(f'Recieved Packet: \n{query}\n')
+        parsed_query = dnslib.DNSRecord.parse(query)
+        print(f'Parsed Packet: \n{parsed_query}\n')
 
-        if 'www.google.com' or 'google.com' in str(question_section):
+        domain_name = str(parsed_query.get_q())
+        # print(f'Domain_name: \n{domain_name}\n')
+
+        if 'playstation.com' in domain_name:
             # print('The website is in the question section of the query.\n')
 
-            skeleton_reply = parsed_packet.reply()
-            skeleton_reply.add_answer(*dnslib.RR.fromZone('www.google.com A 1.2.3.4'))
+            skeleton_reply = parsed_query.reply()
+            skeleton_reply.add_answer(*dnslib.RR.fromZone('playstation.com A 127.0.0.1'))
             print(f'Initiated Response: \n{skeleton_reply}\n')
 
             response = skeleton_reply.pack()
-            server_socket.sendto(response, address)
+            # response = bytes(response)
+            server_socket.sendto(response, client_address)
+            print(f'Sent Response: \n{response}\n')
+
+        # add more elif statements if you need more custom resolutions.
+        # TODO: put custom resolutions into one defined function.
+        elif 'thearchstones.com' in domain_name:
+            
+            skeleton_reply = parsed_query.reply()
+            skeleton_reply.add_answer(*dnslib.RR.fromZone('thearchstones.com A 127.0.0.1'))
+            print(f'Initiated Response: \n{skeleton_reply}\n')
+            
+            response = skeleton_reply.pack()
+            server_socket.sendto(response, client_address)
             print(f'Sent Response: \n{response}\n')
         
-        # TODO: make it a proxy, where only specified Domain Names are resolved the way you want. 
-        # Others should be resolved as per usual (through some public DNS).
-        
+        else: 
+
+            # if it is not in my list of specified domain names above, then query Google's public DNS.
+            # and return the response directly to the client; so this server acts as proxy.
+            client_socket.sendto(query, DNS_address)
+            response = client_socket.recv(1024)
+            server_socket.sendto(response, client_address)
+            print(f'Sent Response from Google DNS: \n{response}\n')
+
+            # pass
+
     except KeyboardInterrupt:
         # raise
         print('Key pressed')
         server_socket.close()
+        client_socket.close()
     except socket.error as err:
         print(err)
         server_socket.close()
+        client_socket.close()
         
--- test_client.py
+++ test_client.py
@@ -3,11 +3,17 @@ import dnslib
 import binascii
 
 # query in hexidecimal
-packet = b'd5ad818000010005000000000377777706676f6f676c6503636f6d0000010001c00c0005000100000005000803777777016cc010c02c0001000100000005000442f95b68c02c0001000100000005000442f95b63c02c0001000100000005000442f95b67c02c0001000100000005000442f95b93'
+# packet = b'd5ad818000010005000000000377777706676f6f676c6503636f6d0000010001c00c0005000100000005000803777777016cc010c02c0001000100000005000442f95b68c02c0001000100000005000442f95b63c02c0001000100000005000442f95b67c02c0001000100000005000442f95b93'
 
 # represented in binary
-packet = binascii.unhexlify(packet)
-print(f'\nInitial binary query packet: \n{packet}\n')
+# packet = binascii.unhexlify(packet)
+# print(f'\nInitial binary query packet: \n{packet}\n')
+
+# test custom resolve on this packet.
+packet1 = b'f\x96\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x0bplaystation\x03com\x00\x00\x01\x00\x01'
+
+# test resolution through public DNS.
+packet2 = b'A\x81\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x02sf\x04prod\x18sonyentertainmentnetwork\x03com\x00\x00\x01\x00\x01'
 
 # create a client UDP socket, (since DNS lookups are done through UDP).
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,8 +24,9 @@ client_socket.settimeout(10)
 server_address = ('127.0.0.1', 53)
 
 try:
-    client_socket.sendto(packet, server_address)
-    response_packet = client_socket.recv(len(packet) + 30)
+    # try sending either packet1 or packet2.
+    client_socket.sendto(packet1, server_address)
+    response_packet = client_socket.recv(1024)
 
     print(f'Resolved packet (in binary): \n{response_packet}\n')
     print(f'Resolved packet (in hexideciaml): \n{binascii.hexlify(response_packet)}\n')
